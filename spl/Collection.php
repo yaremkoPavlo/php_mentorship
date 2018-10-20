bc748023a2610ba38ea8786fcfe2a9e31e952e97
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-10-20 15:04:33 +0300

    add Collection.php

---
+++ spl/Collection.php
@@ -0,0 +1,79 @@
+<?php
+
+class Collection implements JsonSerializable, ArrayAccess, Iterator, Countable
+{
+    protected $collection;
+
+    public function __construct()
+    {
+        $this->collection = [];
+    }
+
+    /**
+     * @param $offset
+     * @return mixed|null
+     */
+    public function __invoke($offset = 0)
+    {
+        return $this->offsetExists($offset) ? $this->collection[$offset] : null;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->collection;
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->collection[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->offsetExists($offset) ? $this->collection[$offset] : null;
+    }
+
+    public function offsetSet($offset, $value):void
+    {
+        if (is_null($offset)) {
+            $this->collection[] = $value;
+        } else {
+            $this->collection[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset):void
+    {
+        unset($this->collection[$offset]);
+    }
+
+    public function current()
+    {
+        return current($this->collection);
+    }
+
+    public function next():void
+    {
+        next($this->collection);
+    }
+
+    public function key()
+    {
+        return key($this->collection);
+    }
+
+    public function valid():bool
+    {
+        return $this->offsetExists($this->current());
+    }
+
+    public function rewind():void
+    {
+        reset($this->collection);
+    }
+
+    public function count()
+    {
+        return count($this->collection);
+    }
+}
---
+++ spl/spl.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors',1);
+error_reporting (E_ALL);
+
+include "Collection.php";
+
+$a = new Collection();
+
+$a[0] = "value";
+$a[1] = "value2";
+$a[8] = "value3";
+
+echo $a[0], PHP_EOL;
+
+echo $a(), PHP_EOL;
+
+echo json_encode($a, JSON_FORCE_OBJECT), PHP_EOL;
+
+foreach ($a as $k => $v) {
+    echo $v;
+}
+
+echo count($a), PHP_EOL;
