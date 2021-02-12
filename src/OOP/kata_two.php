1290e1d033c3a849b99926675f4d3a12ae8b9604
Pavlo Yaremko <Pavlo_Yaremko@epam.com>
2021-02-12 13:29:11 +0200

    mentorship_2: add 1-5 oop katas

---
+++ src/OOP/kata_two.php
@@ -0,0 +1,16 @@
+<?php
+
+class Person
+{
+    public $first_name;
+    public $last_name;
+    public function __construct($first_name, $last_name)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+    }
+    public function get_full_name()
+    {
+        return sprintf("%s %s", $this->first_name, $this->last_name);
+    }
+}
