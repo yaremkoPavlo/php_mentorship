f3f06f739dd767dd9a8aac0e92ee3d5fbf82242c
Pavlo Yaremko <Pavlo_Yaremko@epam.com>
2021-03-26 01:04:08 +0200

    Added configuration for doctrine

---
+++ Container.php
@@ -0,0 +1,37 @@
+<?php
+
+class Container
+{
+    /**
+     * @var array<string,mixed>
+     */
+    private static $container;
+
+    /**
+     * @param string $name
+     * @return mixed
+     * @throws Exception
+     */
+    public static function get(string $name)
+    {
+        if (!array_key_exists($name, static::$container)) {
+            throw new Exception("Can't find service '$name' in container. Please inject it first");
+        }
+
+        return static::$container[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $service
+     * @throws Exception
+     */
+    public static function set(string $name, $service)
+    {
+        if (array_key_exists($name, static::$container)) {
+            throw new Exception("Service '$name' already injected. Please use different name.");
+        }
+
+        static::$container[$name] = $service;
+    }
+}
--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,16 @@
 ini_set('display_errors', 1);
 
 require_once 'vendor/autoload.php';
-require_once 'utils.php';
+require_once 'Container.php';
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$isDevMode = true;
+$paths = [__DIR__ . "/src/DBBase"];
+$dbParams = require_once 'dbparams.php';
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
+Container::set('entityManager', $entityManager);
